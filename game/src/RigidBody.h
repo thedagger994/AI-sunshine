3fcba39ea0951b9aecf0170805e553cc80923ce3
thedagger994 <thedagger994@gmail.com>
2023-06-01 11:15:03 -0400

    Lab 3

--- game/src/RigidBody.h
+++ game/src/RigidBody.h
@@ -8,7 +8,7 @@ struct RigidBody
 	Vector2 acceleration;
 };
 
-void Update(RigidBody rb, float dt)
+void Update(RigidBody &rb, float dt)
 {
 	rb.position = rb.position + rb.velocity * dt + (rb.acceleration * 0.5f * dt * dt);
 	rb.velocity = rb.velocity + rb.acceleration * dt;
--- game/src/SteeringBehaviours.h
+++ game/src/SteeringBehaviours.h
@@ -1,8 +1,9 @@
 #include "Math.h"
+#include "raylib.h"
 
 void Seek(Vector2 agentPosition, Vector2 agentVelocity, Vector2 targetPosition, float desiredSpeed, float acceleration)
 {
 	Vector2 desiredVelocity = Normalize(targetPosition - agentPosition) * desiredSpeed;
-	Vector2 desiredChange = desiredChange - agentVelocity;
-	return Normalize(desiredChange * acceleration);
+	Vector2 desiredChange = desiredChange - (agentVelocity);
+	return Normalize((desiredChange) * acceleration);
 }
